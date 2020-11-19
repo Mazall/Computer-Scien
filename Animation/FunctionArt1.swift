779e793639eefb1b606f0e9720acb9cb486a829d
Mazal <Mzhou@lcs.on.ca>
2020-11-19 14:18:41 -0500

    made a background thought i already had 1

--- Animation/FunctionArt1.swift
+++ Animation/FunctionArt1.swift
@@ -9,11 +9,11 @@ import CanvasGraphics
 
 // NOTE: This is a completely empty sketch; it can be used as a template.
 class FunctionArt1: NSObject, Sketchable {
-
+    
     // NOTE: Every sketch must contain an object of type Canvas named 'canvas'
     //       Therefore, the line immediately below must always be present.
     var canvas: Canvas
-
+    
     // Add many spirals
     // This is now a list, or an array, of functions
     var functions: [MathFunction] = []    // empty list
@@ -23,14 +23,14 @@ class FunctionArt1: NSObject, Sketchable {
         
         // Create canvas object – specify size
         canvas = Canvas(width: 500, height: 500)
-             
+        
         // Initialize many functions
         for i in 1...11 {
             
             // Create the function
             let newFunction = MathFunction(a: 10.0,
                                            k: 15.0,
-                                           d: CGFloat(i) * 20,
+                                           d: CGFloat(i) * 25,
                                            c: 0,
                                            canvas: canvas,
                                            type: .reciprocal)
@@ -43,26 +43,29 @@ class FunctionArt1: NSObject, Sketchable {
         // Speed
         canvas.framesPerSecond = 60
     }
-
+    
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
-        canvas.fillColor = Color(hue: 0, saturation: 100, brightness: 0, alpha: 10)
-    //    canvas.drawRectangle(at: Point(x: 0, y: 0), width: 1000, height: 1000)
+        
+        // Clear the canvas
+        canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 100, alpha: 100)
+        //        canvas.fillColor = Color(hue: 0, saturation: 100, brightness: 0, alpha: 10)
+        canvas.drawRectangle(at: Point(x: 0, y: 0), width: 1000, height: 1000)
         // What frame are we on?
-//        print(canvas.frameCount)
+        //        print(canvas.frameCount)
         
         canvas.defaultLineWidth = 1
         
         // Set the origin to be the middle of the canvas
         canvas.translate(to: Point(x: canvas.width / 2, y: canvas.height / 2))
-
+        
         // Update the position of all functions
         for function in functions {
             function.update(on: canvas,
                             usingInputValue: canvas.frameCount)
         }
-
-    
+        
+        
     }
-
+    
 }
